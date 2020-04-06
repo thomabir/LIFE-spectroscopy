6ce42f3af7c047034557e9e964cf12213cf231c5
Thomas <41652524+thomabir@users.noreply.github.com>
2020-04-06 15:18:13 +0200

    Fix SR, improve plotting

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import poisson
 from functools import lru_cache
 import seaborn as sns; sns.set()
+import itertools as it
 
 @lru_cache(maxsize=128)
 def getflux(time, weather):
@@ -23,8 +24,18 @@ def resample(wl, flux, SR, SNR):
     """Resample the spectrum with the given SR and SNR, and add the standard deviation."""
     wl = np.array(wl)
     flux = np.array(flux)
-    newwl = np.linspace(3., 19., SR)
+
+    lam_min = 3.
+    lam_max = 19.
+    lam_avg = (lam_max - lam_min)/2
+
+    delta_lam = lam_avg/SR
+    n_bins = (lam_max-lam_min)/delta_lam
+    n_bins = int(n_bins)
+
+    newwl = np.linspace(3., 19., n_bins)
     newflux = spectres(newwl, wl, flux)
+
 
     N = SNR**2
     newflux *= N/np.amax(newflux)
@@ -54,27 +65,30 @@ def logprob(hypothesis, data):
     return total_logprob
 
 
-def find_ratio(flux1, flux2, sample):
-    p1 = logprob(flux1, sample)
-    p2 = logprob(flux2, sample)
-    ratios = p1-p2
+def find_probability(hypothesis_flux, data_flux):
+    probs = logprob(hypothesis_flux, data_flux)
+    # ratios = p1-p2
     # plt.hist(ratios)
     # plt.show()
-    ratio = np.median(ratios)
-    return ratio
+    prob = np.median(probs)
+    return prob
+
+# plt.rcParams.update({'lines.markeredgewidth': 1})
 
 def find_probmatrix(SR, SNR, times):
     Ntimes = len(times)
-    probmatrix = np.zeros((Ntimes,Ntimes))
+    probmatrix = np.zeros((Ntimes, Ntimes))
 
     for i, hypothesis_time in enumerate(times):
         for j, data_time in enumerate(times):
-            _, H1_flux, _ = prepare(SR, SNR, time=hypothesis_time, weather='clear')
-            _, H2_flux, _ = prepare(SR, SNR, time=data_time, weather='clear')
-            data_flux = np.around(H2_flux)
+            _, hypothesis_flux, _ = prepare(SR, SNR, time=hypothesis_time, weather='clear')
+            wl, data_flux, _ = prepare(SR, SNR, time=data_time, weather='clear')
+            data_flux = np.around(data_flux)
             data_flux = sample_spectrum(data_flux)
-            probmatrix[i,j] = find_ratio(H1_flux, H2_flux, data_flux)
+            probmatrix[i,j] = find_probability(hypothesis_flux, data_flux)
 
+    for j, _ in enumerate(times):
+        probmatrix[:,j] = probmatrix[:,j]-np.log10( np.sum(10**probmatrix[:,j]) )
 
     return probmatrix
 
@@ -83,29 +97,88 @@ def spectral_distance(probmatrix):
     n = probmatrix.shape[0]
     for i in range(n):
         for j in range(n):
-            distance[i,j] = max(probmatrix[i,j], probmatrix[j,i])
+            # distance[i,j] = np.amax([probmatrix[i,j]-probmatrix[j,j], probmatrix[j,i]-probmatrix[i,i]])
+            distance[i,j] = 10**probmatrix[i,j] - 10**probmatrix[j,j]
             if i < j:
                 distance[i,j] = np.nan
     return np.abs(distance)
 
-weathers = ['clear', 'cloudy']
+def plot_spectra(SR, SNR, scenarios):
+    for (time, weather) in scenarios:
+        wl, flux, err = prepare(SR, SNR, time=time, weather=weather)
+        plt.errorbar(wl, flux, yerr=err, label=time+' '+weather, fmt='o', markersize=8, capsize=8)
+    plt.xlabel('wavelength [um]')
+    plt.ylabel('photon count')
+    plt.title('Earth spectra at different times\nSR = '+str(SR)+', peak SNR = '+str(SNR))
+    plt.legend()
+    # plt.show()
+    plt.savefig('spectra.pdf')
+    plt.close()
+
+class SpectroscopyModel:
+    def __init__(self, SR, SNR, scenarios):
+        self.SR = SR
+        self.SNR = SNR
+        self.scenarios = scenarios
+
+    def get_spectrum(self):
+        pass
+
+    def probmatr(self):
+        pass
+
+    def log_probmatr(self):
+        pass
+
+    def metric1(self):
+        pass
+
+    def metric2(self):
+        pass
+
+weathers = ['clear']#, 'cloudy']
 times = ['modern', '0.8Ga', '2.0Ga', '3.9Ga']
 
+scenarios = it.product(times, weathers)
 
 SR = 10
 SNR = 10
 
+plot_spectra(SR, SNR, scenarios)
+
+# M = SpectroscopyModel(SR, SNR, scenarios)
+# M.probmatr.plot()
+# M.log_probmatr.plot()
+# M.metric1.plot()
+# M.metric2.plot()
+
 probmatrix = find_probmatrix(SR, SNR, times)
+
+def plot_probmatrix(matrix):
+    ax = sns.heatmap(matrix, fmt='.1f', annot=True, cmap=sns.color_palette('Blues_r'))
+    ax.set_xlabel('data')
+    ax.set_ylabel('hypothesis')
+    ax.set_xticklabels(times)
+    ax.set_yticklabels(times)
+    ax.set_title('log Probability of hypothesis, given data\nSR = ' + str(SR) + ', SNR = ' + str(SNR))
+    # fig.tight_layout()
+    # plt.show()
+    plt.savefig('probabilities-SR=' + str(SR) + '-SNR=' + str(SNR) +'.pdf')
+    plt.close()
+
+def plot_distance(matrix):
+    ax = sns.heatmap(distance, fmt='.5f', annot=True, cmap=sns.color_palette('Blues'))
+    ax.set_xticklabels(times)
+    ax.set_yticklabels(times)
+    # ax.set_xlabel('data')
+    # ax.set_ylabel('spectrum')
+    ax.set_title('metric distance between spectra\nSR = ' + str(SR) + ', SNR = ' + str(SNR))
+    # fig.tight_layout()
+    # plt.show()
+    plt.savefig('difference-ratio-SR=' + str(SR) + '-SNR=' + str(SNR) +'.pdf')
+    plt.close()
+
+plot_probmatrix(probmatrix)
+
 distance = spectral_distance(probmatrix)
-
-
-ax = sns.heatmap(distance, fmt='.1f', annot=True, cmap=sns.color_palette('Blues'))
-ax.set_xticklabels(times)
-ax.set_yticklabels(times)
-# ax.set_xlabel('data')
-# ax.set_ylabel('spectrum')
-ax.set_title('metric distance between spectra\nSR = ' + str(SR) + ', SNR = ' + str(SNR))
-# fig.tight_layout()
-plt.show()
-# plt.savefig('SR = ' + str(SR) + ', SNR = ' + str(SNR) +'.pdf')
-# plt.close()
+plot_distance(probmatrix)
