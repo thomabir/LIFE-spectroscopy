feb39c962ff73e359536c81af35e6f45eae5e5c2
Thomas <41652524+thomabir@users.noreply.github.com>
2020-05-21 20:01:10 +0200

    Implement entropy

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ class Spectrum:
     def __init__(self, wl, flux, label):
         self.wl = wl
         self.flux = flux
+        self.fluxerr = 0
         self.label = label
 
         self.original_wl = wl
@@ -63,10 +64,10 @@ class Spectrum:
         newwl = np.linspace(lam_min, lam_max, n_bins)
         newflux = spectres(newwl, self.original_wl, self.original_flux)
 
-        N = SNR**2      # no. of photons per bin
-        newflux *= N / np.amax(newflux)
-
-        newfluxerr = np.sqrt(newflux)
+        if SNR:
+            N = SNR**2      # no. of photons per bin
+            newflux *= N / np.amax(newflux)
+            self.fluxerr = np.sqrt(newflux)
 
         if overwrite_original:
             self.original_wl = newwl
@@ -74,25 +75,30 @@ class Spectrum:
         
         self.wl = newwl
         self.flux = newflux
-        self.fluxerr = newfluxerr
 
     @cached_property
-    def sample_spectrum(self, N=100fi00):
-        flux = np.around(self.flux)
-        return poisson.rvs(flux, size=(N, flux.shape[0]))
+    def sample_spectrum(self):
+        samples = self.samples
+        flux = self.flux
+        return poisson.rvs(flux, size=(samples, flux.shape[0]))
 
     def plot(self, *args, **kwargs):
-        plt.errorbar(self.wl, self.flux, yerr=self.fluxerr,
-                     label=self.label, fmt='o', markersize=8, capsize=8)
+        #plt.errorbar(self.wl, self.flux, yerr=self.fluxerr,
+        #             label=self.label, fmt='o', markersize=8, capsize=8)
+        plt.plot(self.wl, self.flux)
         plt.xlabel('wavelength [um]')
         plt.ylabel('photon count')
         plt.legend()
 
 
 class ProbabilityModel:
-    def __init__(self, spectra):
+    def __init__(self, spectra, samples=150):
         self.spectra = spectra
         self.n = len(spectra)
+        self.samples = samples
+
+        for spectrum in self.spectra:
+            spectrum.samples = samples
 
     @cached_property
     def log_probmatr(self):
@@ -111,14 +117,10 @@ class ProbabilityModel:
 
             # for each sampled spectrum
             total_logprob = np.sum(logprobs, axis=-1)
-            total_logprob *= np.e / 10  # convert to log10 instead of ln
 
-            # average over all sampled spectra
-            # median is used since it is almost invariant under log
-            avg_prob = np.median(total_logprob)
-            return avg_prob
+            return total_logprob
 
-        probmatrix = np.zeros((self.n, self.n))
+        probmatrix = np.zeros((self.n, self.n, self.samples))
 
         # compute the unnormalized log probability matrix
         for i, hypothesis_spectrum in enumerate(spectra):
@@ -127,7 +129,9 @@ class ProbabilityModel:
                 hypothesis_flux = hypothesis_spectrum.flux
                 data_flux = data_spectrum.sample_spectrum
 
-                probmatrix[i, j] = logprob(hypothesis_flux, data_flux)
+                probmatrix[i, j, :] = logprob(hypothesis_flux, data_flux)
+        
+        self.p_x_d = np.copy(probmatrix)
 
         # normalize the log probability matrix.
         for j in range(self.n):
@@ -135,16 +139,17 @@ class ProbabilityModel:
             # for numerical feasability, normalize such that pm[j,j] = 0.
             # this mathematically not necessary, but numerically important,
             # because the range of float64 would be exhausted pretty quickly.
-            probmatrix[:, j] = probmatrix[:, j] - probmatrix[j, j]
+            probmatrix[:, j, :] = probmatrix[:, j, :] - probmatrix[j, j, :]
 
             # normalize each row "properly", such that the probabilites sum up to 1.
-            probmatrix[:, j] = probmatrix[:, j] - np.log10(np.sum(10**probmatrix[:, j]))
+            probmatrix[:, j, :] = probmatrix[:, j, :] - np.log(np.sum(np.e**probmatrix[:, j, :], axis=0))
+
 
         return probmatrix
 
     @cached_property
     def probmatr(self):
-        return 10**self.log_probmatr
+        return np.e**self.log_probmatr
 
     @cached_property
     def metric1(self):
@@ -161,34 +166,6 @@ class ProbabilityModel:
                 if i <= j:
                     distance[i, j] = np.nan
         return np.nanmin(distance)
-
-    @cached_property
-    def metric2(self):
-        n = self.n
-        pm = self.log_probmatr
-        distance = np.zeros(pm.shape)
-
-        for i in range(self.n):
-            for j in range(self.n):
-                if i > j:
-                    d1 = abs(pm[i, j] - pm[j, j])
-                    d2 = abs(pm[j, i] - pm[i, i])
-                    distance[i, j] = min(d1, d2)
-                if i <= j:
-                    distance[i, j] = np.nan
-
-        return np.nanmin(distance)
-
-    @cached_property
-    def metric3(self):
-        """Confidence of most likely hypothesis"""
-        n = self.n
-        pm = self.probmatr
-
-
-        # pm[hypothesis, data]
-        conf = np.nanmax(pm, axis=0)
-        return np.amin(conf)
 
 
 # def plot_probmatrix(matrix):
@@ -237,17 +214,50 @@ for scenario in scenarios:
 def run_simulation(SR=10, SNR=100):
     for spectrum in spectra:
         spectrum.resample(SR=SR, SNR=SNR)
-        try:
-            del spectrum.sample_spectrum
-        except:
-            pass
-        # spectrum.plot()
-    # plt.show()
+
+    samples = 50
 
     # find distance metric
-    p = ProbabilityModel(spectra)
-    result = p.metric1
-    return 1 - result
+    p = ProbabilityModel(spectra, samples=samples)
+    pm = p.log_probmatr
+    p_x_d = p.p_x_d
+
+    newpm = np.zeros((4,4*samples))
+    new_p_x_d = np.zeros((4, 4*samples))
+    
+    for i in range(4):
+        newpm[i, :] = pm[i, :, :].flatten()
+        new_p_x_d[i, :] = p_x_d[i, :, :].flatten()
+    
+    #print('entropy before:', -np.log(1/4))
+    E_before = -np.log(1/4)
+
+    P = newpm
+
+    P_x_d = new_p_x_d
+
+    P_x_d -= np.mean(P_x_d)    # normalize for better numerics
+    P_x_d = np.sum(np.e**P_x_d, axis=0)
+    P_x_d = P_x_d / np.sum(P_x_d) # normalize so that it sums to 1
+
+    U_x_d = - P * np.exp(P)
+    U_x_d = np.sum(U_x_d, axis=0)
+
+
+
+    
+
+    U_d = P_x_d * U_x_d
+    U_d = np.sum(U_d)
+    #print('entropy after:', U_d)
+    E_after = U_d
+
+    utility_gain = E_before - E_after
+
+    for spectrum in spectra:
+        del spectrum.sample_spectrum
+
+    return E_after
 
 
 def plot_heatmap(dist, title):
@@ -279,42 +289,63 @@ def generate_heatmap(all_SNR=10, all_SR=100):
     title = '1 - difference between probabilities'
     plot_heatmap(dist, title)
 
+
+from scipy.ndimage import median_filter
+
 # to increase preformance for high-res original spectra,
 # resample them with SR = 1000
 for spectrum in spectra:
-    spectrum.resample(SNR=100, SR=2000, overwrite_original=True)
+    spectrum.resample(SNR=None, SR=2000, overwrite_original=True)
 
 
-exptimes = [1000, 1500, 2000]
+
+
+
+exptimes = [500, 1000, 2000, 3000]
 for exptime in exptimes:
-    all_SNR = np.linspace(2,10,20)
-    all_SR = exptime / all_SNR**2
+    all_SR = np.geomspace(6, 1000, num=20)
+    all_SNR = np.sqrt(exptime / all_SR)
 
-    dist = np.zeros(all_SNR.shape)
+    # all_SNR = np.geomspace(0.1,20, num=20)
+    # all_SR = exptime / all_SNR**2
+    # idx = (5 < all_SR) & (all_SR < 200)
+    # all_SNR = all_SNR[idx]
+    # all_SR = all_SR[idx]
+    # print(all_SR)
+
+    dist_low = np.zeros(all_SNR.shape)
+    dist_high = np.zeros_like(dist_low)
+    dist_med = np.zeros_like(dist_low)
+
     for i in range(all_SNR.shape[0]):
-            dist[i] = run_simulation(SR=all_SR[i], SNR=all_SNR[i])
+        errs = np.zeros((5))
+        for N in range(5):  # no of experiments, to determine error
+            errs[N] = run_simulation(SR=all_SR[i], SNR=all_SNR[i])
 
-    dist = dist / np.mean(dist)
-    #print(all_SR)
+        dist_low[i] = np.percentile(errs, 20)
+        dist_med[i] = np.median(errs)
+        dist_high[i] = np.percentile(errs, 80)
 
-    plt.plot(all_SNR, dist, label=exptime)
+    plt.subplot(121)
+    plt.fill_between(all_SR, dist_low, dist_high, label=exptime, alpha=0.2)
+    plt.plot(all_SR, dist_med)
+    plt.xlabel('SR')
+    plt.yscale('log')
+    plt.xscale('log')
+    plt.ylabel('posterior entropy')
+    plt.title('Ideal SR for different exposure times')
+    plt.legend()
+
+    plt.subplot(122)
+    plt.fill_between(all_SNR, dist_low, dist_high, label=exptime, alpha=0.2)
+    plt.plot(all_SNR, dist_med)
     plt.xlabel('SNR')
-    plt.ylabel('goodness of experiment, normalized\n(lower is better)')
-    plt.title('Ideal SNR for 3 different exposure times')
+    plt.yscale('log')
+    plt.ylabel('posterior entropy')
+    plt.title('Ideal SNR for different exposure times')
     plt.legend()
 
 plt.show()
-
-
-#N_tot = SNR**2 * SR
-#all_SR = 500 / np.array(all_SNR)**2
-#all_SR = [100, 50, 20, 10]
-# all_SNR = np.sqrt(2000 / np.array(all_SR))
-
-
-# all_SR = [10, 20, 50, 100]
-# all_SNR = [2, 3, 5, 7, 10, 20]
-# generate_heatmap(all_SNR=all_SNR, all_SR=all_SR)
 
 
 
